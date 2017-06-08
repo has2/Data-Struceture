994e1f06259208eb54a53f2a816330dd24b4559f
2012003567 <lsh111112@naver.com>
2017-06-08 14:16:05 +0900

    dfs

---
+++ DS2/p10_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#define MAX_VERTICES 4 
+
+int adj_mat[MAX_VERTICES][MAX_VERTICES]={ 
+	{0,1,0,1}, 
+	{1,0,1,1}, 
+	{0,1,0,1}, 
+	{1,1,1,0} 
+}; 
+int visited[MAX_VERTICES]; 
+int n=4;
+
+void graph_dfs_mat(int v) { 
+	int w; 
+	visited[v] = 1; 
+	printf("%d ", v); 
+	for (w = 0; w<n; w++) { 
+		if (adj_mat[v][w]&&!visited[w]) {
+			graph_dfs_mat(w);
+		}
+	}
+}
+
+void main() { 
+	graph_dfs_mat(0); 
+}
